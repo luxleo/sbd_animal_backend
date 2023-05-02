9c2162f4a5526daedaec3169b61038864c02ecb9
Dong Han <lux00leo@gmail.com>
2023-05-02 19:53:48 +0900

    md pic test

--- sbd_animal/sbd_animal/accounts/signals.py
+++ sbd_animal/sbd_animal/accounts/signals.py
@@ -1,6 +1,7 @@
 from django.dispatch import receiver
 from django.db.models.signals import pre_save, post_delete
 from .models import User
+from django.conf import settings
 
 
 @receiver(pre_save, sender=User)
@@ -8,16 +9,40 @@ def pre_save_profile(sender, instance, *args, **kwargs):
     """
     old profile image delete before profile edited with new profile image
     """
-    try:
-        old_avatar = instance.__class__.objects.get(id=instance.id).avatar.path
+    if settings.DEBUG:
         try:
-            new_avatar = instance.avatar.path
-        except:
-            new_avatar = None
-        if new_avatar != old_avatar:
-            import os
+            old_avatar = instance.__class__.objects.get(id=instance.id).avatar.path
 
-            if os.path.exists(old_avatar):
-                os.remove(old_avatar)
-    except:
-        pass
+            # 프로필 설정했을 때
+            try:
+                new_avatar = instance.avatar.path
+
+            # 프로필 설정 안했을때
+            except:
+                new_avatar = None
+            if new_avatar != old_avatar:
+                import os
+
+                if os.path.exists(old_avatar):
+                    os.remove(old_avatar)
+        except:
+            pass
+    else:
+        try:
+            # prod 모드에서는 path로 접근시 오류발생
+            old_avatar = instance.__class__.objects.get(id=instance.id).avatar.name
+            try:
+                new_avatar = instance.avatar.name
+            except:
+                new_avatar = None
+            if new_avatar != old_avatar:
+                # s3 Storage
+                from django.core.files.storage import default_storage
+
+                try:
+                    if default_storage.exists(old_avatar):
+                        default_storage.delete(old_avatar)
+                except:
+                    pass
+        except:
+            pass
